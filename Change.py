18e8942b559e6bfa39df630b4040428b2455cfab
Thomas Chastaingt <thomas-chastaingt@outlook.com>
2020-11-20 18:25:07 +0100

    init

---
+++ Change.py
@@ -0,0 +1,23 @@
+coins_values = [5,10,50,100]
+itemValue = 275
+paidValue = 300
+
+def Change(itemValue, paidValue, coins_values):
+    map = {}
+    render = paidValue - itemValue
+
+    #add every items in map form coins_values
+    for c in coins_values:
+        map.update({c : 0})
+
+    i = len(coins_values) - 1
+    while render > 0:
+        val = coins_values[i]
+        if render < val:
+            i -= 1
+        else:
+            map[val] += 1
+            render -= val
+    print(map)
+
+Change(itemValue, paidValue, coins_values)
